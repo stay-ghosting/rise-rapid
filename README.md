8eac86212631561f9ef81c882ca9e8352dd0104d
Rheon Mais <rheonmais@gmail.com>
2024-08-26 01:03:34 +0100

    fixed footer layout

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 
 ## TODO
 # in order
-- make footer full width
 - remove debug tools
 - stop form from getting spamed
 - make arrow maller and not above the lines
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -25,40 +25,42 @@ const Footer = () => {
   const [email, setEmail] = useState("");
 
   return (
-    <div className='bg-[#151515] pb-6 flex flex-col items-center '>
-      <div className='hidden sm:block px-16'>
-        <ul className='flex space-x-[5rem] text-white font-extralight mt-16'>
-          <CustomNavLink link='/' text='Home' key="1"/>
-          <CustomNavLink link='/contact' text='Contact' key="2"/>
-          <CustomNavLink link='/about' text='About' key="3"/>
-          <CustomNavLink link='/blog' text='Blog' key="4"/>
-          <CustomNavLink link='/terms' text='Terms' key="5"/>
-          <CustomNavLink link='/privacy' text='Privacy' key="6"/>
-        </ul>
-        <div className='relative md:mt-[11rem] mb-[6rem]  flex flex-col justify-center items-center'>
-          <div className=' md:absolute -left-[9rem] -top-[7rem]'>
-            <p className='mt-[3rem] mb-[2rem] md:my-0 text-white text-[4rem] font-bold leading-[6rem]'>
-              NEWSLETTER
-              <span className='text-cta md:block'> SIGNUP</span>
-            </p>
-          </div>
-          <div className='w-[36rem]  relative'>
-            <form action=''>
-              <input type='image' src={icon_submitArrow} className='cursor-pointer p-[1rem] w-[4.5rem] absolute right-2 top-2' />
-              <InputField
-                fieldValue={email}
-                setFieldValue={(name, value) => setEmail(value)}
-                id='email'
-                name='email'
-                type='email'
-                placeholder='Your email address'
-                autoComplete='email'
-              />
-            </form>
+    <div className='bg-[#151515] pb-6 flex flex-col items-center w-full'>
+      <div className='hidden sm:block w-full'>
+        <div className="flex flex-col items-center">
+          <ul className='flex space-x-[5rem] text-white font-extralight mt-16'>
+            <CustomNavLink link='/' text='Home' key='1' />
+            <CustomNavLink link='/contact' text='Contact' key='2' />
+            <CustomNavLink link='/about' text='About' key='3' />
+            <CustomNavLink link='/blog' text='Blog' key='4' />
+            <CustomNavLink link='/terms' text='Terms' key='5' />
+            <CustomNavLink link='/privacy' text='Privacy' key='6' />
+          </ul>
+          <div className='relative md:mt-[11rem] mb-[6rem]  flex flex-col justify-center items-center'>
+            <div className=' md:absolute -left-[16rem] -top-[7rem]'>
+              <p className='mt-[3rem] mb-[2rem] md:my-0 text-white text-[4rem] font-bold leading-[6rem]'>
+                NEWSLETTER
+                <span className='text-cta md:block'> SIGNUP</span>
+              </p>
+            </div>
+            <div className='w-[36rem]  relative'>
+              <form action=''>
+                <input type='image' src={icon_submitArrow} className='cursor-pointer p-[1rem] w-[4.5rem] absolute right-2 top-2' />
+                <InputField
+                  fieldValue={email}
+                  setFieldValue={(name, value) => setEmail(value)}
+                  id='email'
+                  name='email'
+                  type='email'
+                  placeholder='Your email address'
+                  autoComplete='email'
+                />
+              </form>
+            </div>
           </div>
         </div>
         <div className='border-[#505050] border-b w-full'></div>
-        <div className='flex justify-between items-center w-full mt-6'>
+        <div className='flex justify-between items-center px-[10%] w-full mt-6'>
           <p className='font-thin text-[0.875rem] text-white'>Established in 2024. All rights reserved.</p>
           <div className='flex space-x-8'>
             {socialIcons.map(({ image, altText }, index) => (
@@ -69,8 +71,8 @@ const Footer = () => {
       </div>
       {/* small version */}
       <div className='block sm:hidden w-full'>
-        <p className="text-center">
-        <span className='text-cta'>Subscribe</span> to our newsletter
+        <p className='text-center'>
+          <span className='text-cta'>Subscribe</span> to our newsletter
         </p>
         <div className='bg-car py-[4rem] px-[1rem] relative'>
           <form action=''>
@@ -87,22 +89,22 @@ const Footer = () => {
           </form>
         </div>
         <ul className='flex flex-col items-center space-y-[1rem] text-white font-extralight mt-16'>
-          <CustomNavLink link='/' text='Home' key="1"/>
-          <CustomNavLink link='/contact' text='Contact' key="2"/>
-          <CustomNavLink link='/about' text='About' key="3"/>
-          <CustomNavLink link='/blog' text='Blog' key="4"/>
-          <CustomNavLink link='/terms' text='Terms' key="5"/>
-          <CustomNavLink link='/privacy' text='Privacy' key="6"/>
+          <CustomNavLink link='/' text='Home' key='1' />
+          <CustomNavLink link='/contact' text='Contact' key='2' />
+          <CustomNavLink link='/about' text='About' key='3' />
+          <CustomNavLink link='/blog' text='Blog' key='4' />
+          <CustomNavLink link='/terms' text='Terms' key='5' />
+          <CustomNavLink link='/privacy' text='Privacy' key='6' />
         </ul>
 
         <div className='border-[#505050] border-b w-full'></div>
-          <div className='flex justify-center py-[1rem] space-x-8'>
-            {socialIcons.map(({ image, altText }, index) => (
-              <img src={image} alt={altText} key={index} className='w-12 h-12 object-contain' />
-            ))}
-          </div>
-          <p className='font-thin text-[0.875rem] text-center text-white'>Established in 2024. All rights reserved.</p>
+        <div className='flex justify-center py-[1rem] space-x-8'>
+          {socialIcons.map(({ image, altText }, index) => (
+            <img src={image} alt={altText} key={index} className='w-12 h-12 object-contain' />
+          ))}
         </div>
+        <p className='font-thin text-[0.875rem] text-center text-white'>Established in 2024. All rights reserved.</p>
+      </div>
     </div>
   );
 };
